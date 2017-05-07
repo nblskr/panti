d2011ba341a60e24e0c883e2b3e49281e25e9abe
nblskr <nabila.s.ramadhanti@gmail.com>
2017-05-07 17:00:35 +0700

    login+home

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+import { LoginComponent } from './login/login.component';
 
 @Component({
   selector: 'my-app',
-  template: `<h1>Hello {{name}}</h1>`,
+  templateUrl: './app.component.html',
+  styleUrls: [ './app.component.css' ]
 })
-export class AppComponent  { name = 'Angular'; }
+
+export class AppComponent{ 
+  name = 'Angular'; 
+  
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,12 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
+
 
 import { AppComponent }  from './app.component';
 
 @NgModule({
-  imports:      [ BrowserModule ],
+  imports:      [ BrowserModule, RouterModule ],
   declarations: [ AppComponent ],
   bootstrap:    [ AppComponent ]
 })
