fa7dd24fb2d6cf5f36535233bd8e620d4ba5bcf2
unknown <selfiqisthina@yahoo.com>
2017-05-25 14:43:31 +0700

    tambahan regis+db

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var bodyParser=require('body-parser');
 var app = express();
 var authenticateController=require('./controllers/authenticate-controllers');
 var registerController=require('./controllers/registration-controllers');
+var regispantiController=require('./controllers/regispanti-controllers');
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(bodyParser.json());
 
@@ -10,7 +11,8 @@ app.use(bodyParser.json());
 /* route to handle login and registration */
 
 app.post('/authenticate-controllers',authenticateController.authenticate);
-app.post('/registration-controllers', registerController.register);	
+app.post('/registration-controllers', registerController.register);
+app.post('/regispanti-controllers', regispantiController.register);	
 
 app.get('/',function(req, res){   
       res.sendFile(__dirname + '/index.html');
